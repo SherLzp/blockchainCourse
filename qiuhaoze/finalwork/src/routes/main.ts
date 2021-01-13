b75bf369bd3d21ffa7b228d4d4bfa8626d502653
GreatWangZai <75625571+GreatWangZai@users.noreply.github.com>
2021-01-13 16:51:30 +0800

    Delete main.ts

--- qiuhaoze/finalwork/src/routes/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './routes'
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-createApp(App).use(router).use(Antd).mount('#app')
